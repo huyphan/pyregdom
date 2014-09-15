59edc2ff6212b58c4a1e8aa008f581b63842bc57
Huy Phan <dachuy@gmail.com>
2014-09-15 13:28:09 +0800

    Bumped to version 0.3

--- regdom/__init__.py
+++ regdom/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'regdom'
-__version__ = '0.2'
-__build__ = 0x03
+__version__ = '0.3'
+__build__ = 0x04
 __author__ = 'Huy Phan'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2014 Huy Phan'
